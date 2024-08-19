fb6c7cdd7f18bf58f4b9ffbe8e1a7aae8c241cf9
chris <christophe@math.aau.dk>
2024-08-19 16:14:15 +0200

    default threshold to smallest pixelarea of covariates

---
+++ R/utils.R
@@ -0,0 +1,17 @@
+
+#' Find the small pixel area in list of images
+#'
+#' @param x 
+#'
+#' @return A number
+#' @export
+#'
+#' @examples
+smallest_pixelarea <- function(x) {
+  allarea <- sapply(x, FUN = function(i) {
+    y <- unclass(i)[c("xstep", "ystep")]
+    pixelarea <- y$xstep * y$ystep
+  })
+
+  return(min(allarea))
+}

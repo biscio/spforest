db5ab11e9da358a7b979e97f55b3225dce0c96a2
Christophe Biscio <christophe@math.aau.dk>
2024-08-29 22:32:25 +0200

    help processing dataset

--- R/data.R
+++ R/data.R
@@ -15,6 +15,38 @@
 #' @title soil data for bei
 #'
 #' @description Contains the soil data of bei put at the same resolution
+#'It has been obtained with the following code 
+# beisoil <- list(
+#   bei.extra$elev, bei.extra$grad,
+#   Al, B, Ca, Cu, Fe, K, Mg, Mn, P, Zn, N, Nmin, pH
+# )
+# 
+# ## Put them in the same resolution ----
+# beisoilres <- NULL
+# beisoilres[[1]] <- beisoil[[1]]
+# beisoilres[[2]] <- beisoil[[2]]
+# for (i in 3:15) {
+#   W <- commonGrid(beisoil[[1]], beisoil[[i]])
+#   beisoilres[[i]] <- beisoil[[i]][W, drop = F]
+#   rm(W)
+# }
+# names(beisoilres) <- c(
+#   "elev", "grad", "Al", "B", "Ca",
+#   "Cu", "Fe", "K", "Mg", "Mn",
+#   "P", "Zn", "N", "Nmin", "pH"
+# )
+# 
+# ## center and scale
+# beisoilnorm <- lapply(beisoilres, FUN = function(i) {
+#   (i - mean(i)) / sd(i)
+# })
+# 
+# names(beisoilnorm) <- c(
+#   "elev", "grad", "Al", "B", "Ca",
+#   "Cu", "Fe", "K", "Mg", "Mn",
+#   "P", "Zn", "N", "Nmin", "pH"
+# )
+
 #'
 #' @format A list of 15 \code{im} with same resolutions, and given in the
 #' same order as in Jeff's paper

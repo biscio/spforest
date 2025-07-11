34ad6ae95d8fdde36e8dda0daf28d201096c11e8
christophe <christophe@math.aau.dk>
2025-07-11 22:01:18 +0200

    add more tests

---
+++ inst/tinytest/test_perf.R
@@ -0,0 +1,33 @@
+Mnsin <- 1 + sin(spforest::beisoilres[[10]] / 50)
+z500sin <- 500 * Mnsin / integral(Mnsin)
+X <- spatstat.random::rpoispp(lambda = z500sin, nsim = 1)
+
+
+foo1 <- spforest(
+  X = X,
+  listcovariates = spforest::beisoilres[sample(15)],
+  Ntree = 200,
+  mtry = 0.5,
+  minpts = 100,
+  randmtry = TRUE,
+  p = 0,
+  score = "lcv",
+  threshold = 50,
+  cores = 2
+)
+
+expect_true(max(abs(as.im(foo1) - z500sin)) < 0.00128)
+
+vipfoo1 <- sapply(
+  X = seq_along(foo1$listcov),
+  FUN = function(i) {
+    mean(importance(foo1,
+                    id_cov = i,
+                    viptype = 4
+    ))
+  }
+)
+names(vipfoo1) <- names(foo1$listcov)
+
+expect_equal(names(which.max(abs(vipfoo1))), "Mn")
+
--- inst/tinytest/test_spforest.R
+++ inst/tinytest/test_spforest.R
@@ -1,105 +1,105 @@
-# # Test spforest call tesscovforest ----
-# 
-# expect_silent(
-#   forest <- spforest(
-#     X = spatstat.data::bei,
-#     listcovariates = spatstat.data::bei.extra,
-#     score = "lcv",
-#     p = 1 / 2,
-#     Ntree = 2,
-#     threshold = 0.01,
-#     cores = 1,
-#     mtry = 1 / 3,
-#     randmtry = TRUE,
-#     minpts = 100
-#   )
-# )
-# 
-# expect_equal(class(forest), "spforest")
-# 
-# 
-# # Test spforest with tesscovforest - other parameters ----
-# 
-# ## mtry to choose a fixed number of param each times ---- 
-# 
-# expect_silent(
-#   forest <- spforest(
-#     X = spatstat.data::bei,
-#     listcovariates = beisoilres,
-#     score = "lcv",
-#     p = 1 / 2,
-#     Ntree = 2,
-#     threshold = 0.01,
-#     cores = 1,
-#     mtry = 8,
-#     randmtry = FALSE,
-#     minpts = 100
-#   )
-# )
-# 
-# 
-# # Test spforest call tessforest ----
-# 
-# expect_silent(
-#   forest2 <- spforest(
-#     X = spatstat.data::bei,
-#     Ntree = 5,
-#     lambda = 50,
-#     dimyx = c(51, 101),
-#     test.connected = FALSE,
-#     cores = 1
-#   )
-# )
-# 
-# expect_equal(class(forest2), "spforest")
-# 
-# expect_silent(
-#   extractforest(forest = forest2, whichtrees = c(1, 3))
-# )
-# 
-# # Test method for spforest from tessforest ----
-# 
-# expect_silent(print(forest2))
-# expect_silent(plot(forest))
-# 
-# # Test method for spforest from tesscovforest ----
-# 
-# expect_silent(print(forest))
-# 
-# expect_length(predict(forest), spatstat.geom::npoints(forest$X))
-# expect_true(is.numeric(predict(forest)))
-# expect_length(predict(
-#   object = forest,
-#   newdata = spatstat.random::runifpoint(
-#     n = 50,
-#     win = forest$X$window
-#   )
-# ), 50)
-# expect_true(is.numeric(
-#   predict(
-#     object = forest,
-#     newdata = spatstat.random::runifpoint(
-#       n = 50,
-#       win = forest$X$window
-#     )
-#   )
-# ))
-# expect_null(predict(
-#   object = forest,
-#   newdata = spatstat.random::runifpoint(n = 0)
-# ))
-# expect_true(is.numeric(predict(forest,
-#   newdata = c(827, 319)
-# )))
-# expect_true(is.numeric(predict(forest,
-#   newdata = cbind(
-#     c(827, 319),
-#     c(418, 88)
-#   )
-# )))
-# 
-# # Testing boxplot and vipplot methods for spforest ----
-# 
+# Test spforest call tesscovforest ----
+
+expect_silent(
+  forest <- spforest(
+    X = spatstat.data::bei,
+    listcovariates = spatstat.data::bei.extra,
+    score = "lcv",
+    p = 1 / 2,
+    Ntree = 2,
+    threshold = 0.01,
+    cores = 1,
+    mtry = 1 / 3,
+    randmtry = TRUE,
+    minpts = 100
+  )
+)
+
+expect_equal(class(forest), "spforest")
+
+
+# Test spforest with tesscovforest - other parameters ----
+
+## mtry to choose a fixed number of param each times ----
+
+expect_silent(
+  forest <- spforest(
+    X = spatstat.data::bei,
+    listcovariates = beisoilres,
+    score = "lcv",
+    p = 1 / 2,
+    Ntree = 2,
+    threshold = 0.01,
+    cores = 1,
+    mtry = 8,
+    randmtry = FALSE,
+    minpts = 100
+  )
+)
+
+
+# Test spforest call tessforest ----
+
+expect_silent(
+  forest2 <- spforest(
+    X = spatstat.data::bei,
+    Ntree = 5,
+    lambda = 50,
+    dimyx = c(51, 101),
+    test.connected = FALSE,
+    cores = 1
+  )
+)
+
+expect_equal(class(forest2), "spforest")
+
+expect_silent(
+  extractforest(forest = forest2, whichtrees = c(1, 3))
+)
+
+# Test method for spforest from tessforest ----
+
+expect_silent(print(forest2))
+expect_silent(plot(forest))
+
+# Test method for spforest from tesscovforest ----
+
+expect_silent(print(forest))
+
+expect_length(predict(forest), spatstat.geom::npoints(forest$X))
+expect_true(is.numeric(predict(forest)))
+expect_length(predict(
+  object = forest,
+  newdata = spatstat.random::runifpoint(
+    n = 50,
+    win = forest$X$window
+  )
+), 50)
+expect_true(is.numeric(
+  predict(
+    object = forest,
+    newdata = spatstat.random::runifpoint(
+      n = 50,
+      win = forest$X$window
+    )
+  )
+))
+expect_null(predict(
+  object = forest,
+  newdata = spatstat.random::runifpoint(n = 0)
+))
+expect_true(is.numeric(predict(forest,
+  newdata = c(827, 319)
+)))
+expect_true(is.numeric(predict(forest,
+  newdata = cbind(
+    c(827, 319),
+    c(418, 88)
+  )
+)))
+
+# Testing boxplot and vipplot methods for spforest ----
+
 # expect_silent(boxplot(forest))
 # 
 # 
@@ -107,4 +107,4 @@
 # expect_silent(vipplot(forest, sorted = FALSE))
 # expect_true(vipplot(forest, sorted = TRUE)$Importance[1] >
 #   vipplot(forest, sorted = TRUE)$Importance[2])
-# 
+

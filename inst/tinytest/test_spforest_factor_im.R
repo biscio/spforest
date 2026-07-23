ab2d891b1e9a13a1e5eb50725fd6d4b12114d173
chris <christophe@math.aau.dk>
2026-07-23 09:56:13 +0200

    Add test for factor image

---
+++ inst/tinytest/test_spforest_factor_im.R
@@ -0,0 +1,30 @@
+
+expect_silent(
+  facbei <- lapply(beisoilres, FUN = function(i) {
+    W <- spatstat.geom::cut.im(i,
+                breaks = quantile(i),
+                include.lowest = T
+    )
+    
+    levels(W) <- letters[1:4]
+    return(W)
+  })
+)
+
+
+expect_true(
+  is.im(imfcttoreal(Z=cut(beisoilres[[1]], 
+                    quantile(beisoilres[[1]]), 
+                    include.lowest = T), 
+              X=spatstat.data::bei))
+)
+
+expect_silent(
+  foo <- spforest(X = bei, 
+                  listcovariates = facbei,
+                  Ntree = 2,
+                  mtry = 10,
+                  minpts = 400,
+                  parallel = F)
+)
+

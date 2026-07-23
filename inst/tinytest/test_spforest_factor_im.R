325fac9a231709cc626c0dde5cb23d3825236dee
chris <christophe@math.aau.dk>
2026-07-23 13:16:25 +0200

    more test

--- inst/tinytest/test_spforest_factor_im.R
+++ inst/tinytest/test_spforest_factor_im.R
@@ -1,30 +1,40 @@
+expect_silent(
+  convertedim <- imfcttoreal(
+    Z = cut(beisoilres[[1]],
+      quantile(beisoilres[[1]]),
+      include.lowest = T
+    ),
+    X = spatstat.data::bei
+  )
+)
+
+expect_true(
+  is.im(convertedim)
+)
+
+expect_true(
+  convertedim$type == "real"
+)
 
 expect_silent(
   facbei <- lapply(beisoilres, FUN = function(i) {
     W <- spatstat.geom::cut.im(i,
-                breaks = quantile(i),
-                include.lowest = T
+      breaks = quantile(i),
+      include.lowest = T
     )
-    
+
     levels(W) <- letters[1:4]
     return(W)
   })
 )
 
-
-expect_true(
-  is.im(imfcttoreal(Z=cut(beisoilres[[1]], 
-                    quantile(beisoilres[[1]]), 
-                    include.lowest = T), 
-              X=spatstat.data::bei))
-)
-
 expect_silent(
-  foo <- spforest(X = bei, 
-                  listcovariates = facbei,
-                  Ntree = 2,
-                  mtry = 10,
-                  minpts = 400,
-                  parallel = F)
+  foo <- spforest(
+    X = bei,
+    listcovariates = facbei,
+    Ntree = 2,
+    mtry = 10,
+    minpts = 400,
+    parallel = F
+  )
 )
-
